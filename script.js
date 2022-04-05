d468c1eaad5a010d8a27d75539113ad4300e9a07
Patricia Edwards <patriciaje16@gmail.com>
2022-04-05 18:10:03 +0100

    added event listener to board and reset

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /////first thing is to select all of our cell elements///
-const squares = document.getElementsByClassName("cell");
+const squares = document.querySelectorAll(".cell");
 
 ////select our game status display///
 const gameDisplay = document.querySelector(".gameContainer")
@@ -13,6 +13,25 @@ const resetButton = document.querySelector(".restartButton")
 
 ////////////variable to track to track played cells and validate game state later on///////
 
+let board = ['', '', '', '', '', '', '', '', ''];
+/* Index within the board
+        [0] [1] [2]
+        [3] [4] [5]
+        [6] [7] [8]
+        
+ *////////
+
+/////area to store player/////
+let currentPlayer = 'X';
+
+/////place to store end game result
+let isGameActive = true;
+
+
+////variables to represent end game state////////////////
+const playerXWon = 'Player x Won';
+const playerOWon = 'Player O Won';
+const tie = 'Tie';
 
 
 // you need to record someone has clicked something in an array///then have a head function to check array to see if anyone has won////
@@ -21,10 +40,15 @@ const resetButton = document.querySelector(".restartButton")
 //////messages to display to user during the game such as winning message, draw message, current player turn//////
 
 
-////variable for current player///////
-
-// player x and player 0
 ////////setting message to let players know whose turn it is/////
 
 
-///adding event listeners to game cells and restart button 
+///adding event listeners to game cells 
+squares.forEach( (cell) => {
+  cell.addEventListener('click', () => console.log("It works") );
+});
+
+
+
+///adding event listners to reset button
+resetButton.addEventListener('click', () => console.log("It works") );
