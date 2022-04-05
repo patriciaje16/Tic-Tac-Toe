ea7570e3bc1e1ecbae01c0ea0ae4902495b01b1b
Patricia Edwards <patriciaje16@gmail.com>
2022-04-05 17:24:14 +0100

    added index file and styling

---
+++ script.js
@@ -0,0 +1,21 @@
+/////first thing is to select all of our cell elements///
+
+
+
+/////game status element///////// ***Here we declare some variables that we will use to track the game state throught the game///
+
+
+////////////variable to track to track played cells and validate game state later on///////
+// you need to record someone has clicked something in an array///then have a head function to check array to see if anyone has won////
+
+
+//////messages to display to user during the game such as winning message, draw message, current player turn//////
+
+
+////variable for current player///////
+
+// player x and player 0
+////////setting message to let players know whose turn it is/////
+
+
+///adding event listeners to game cells and restart button 
